321f22b0d4abdd87b49035b452546f2b2cb4753a
dhDSouza <dhs.danielsouza@gmail.com>
2026-02-11 11:24:07 -0300

    Importação das rotas de usuário

--- src/server.js
+++ src/server.js
@@ -1,13 +1,15 @@
 import express from 'express'
 import cors from 'cors'
 import { PORT } from './config/env-config.js'
+import usuarioRoutes from './routes/usuario-routes.js'
 
 const app = express()
-const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
+
+app.use(usuarioRoutes)
 
 app.listen(PORT, ()=> {
     console.log(`Servidor rodando em http://localhost:${PORT}`)
